48177c04d1107ab8ecbd45fbc80ed143ee2ab5f2
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-09-11 20:06:02 -0300

    Add files via upload

---
+++ project-nutri/script.js
@@ -0,0 +1,43 @@
+export const thead = {
+  thead1: "Calorias (valor energético)",
+  thead2: 1431.0,
+  thead3: 71.55,
+};
+
+export const tbody = [
+  {
+    tbody1: "Carboidratos líquidos",
+    tbody2: 0.0,
+    tbody3: "-",
+  },
+  {
+    tbody1: "Carboidratos",
+    tbody2: 0.0,
+    tbody3: 0.0,
+  },
+  {
+    tbody1: "Proteinas",
+    tbody2: 32.0,
+    tbody3: 10.67,
+  },
+  {
+    tbody1: "Gorduras totais",
+    tbody2: 2.5,
+    tbody3: 4.55,
+  },
+  {
+    tbody1: "Gorduras saturadas",
+    tbody2: 0.9,
+    tbody3: 4.09,
+  },
+  {
+    tbody1: "Fibra alimentar",
+    tbody2: 0.0,
+    tbody3: 0.0,
+  },
+  {
+    tbody1: "Sodio",
+    tbody2: 50.0,
+    tbody3: 2.08,
+  },
+];
